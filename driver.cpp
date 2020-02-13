4547fbff2d97e53a1606317eb8f8d33b5f5a7793
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-02-13 22:02:34 +0200

    Added infinite "event loop" to main method with working interface options.

--- driver.cpp
+++ driver.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
+#include <cstdlib>
+#include <string>
 
 int main(void)
 {
+    for (;;)
+    {
+        std::cout << "\n0: Add Student\n\
+    1: Read Database\n\
+    2: Save Database\n\
+    3: Display Given Student Data\n\
+    4: Grade Given Student\n\
+    q: Quit\n\n\
+    Enter a given number (or q to quit) and press return: ";
+
+        std::string entered;
+        std::cin >> entered;
+
+        std::cout << entered;
+
+        if (entered.compare("q") == 0)
+        {
+            break;
+        }
+    }
 
     return 0;
 }
