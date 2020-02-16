b4013542e59e9b39b34292499e5f71a50b45e9a5
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-02-16 13:16:49 +0200

    Added "Student" structure to database.h to hold the details of a particular student record as well as a vector of type "Student" to temporarily store the database of Student records.

--- database.h
+++ database.h
@@ -2,9 +2,21 @@
 #define database_h
 
 #include <string>
+#include <vector>
+#include <fstream>
 
 namespace CHNJAR003
 {
+
+struct Student
+{
+    std::string fName;
+    std::string sName;
+    std::string studentNumber;
+    std::string classRecord;
+};
+
+std::vector<Student> StudentRecords;
 
 void addStudent(const std::string fName, const std::string sName, const std::string studentNumber, const std::string classRecord);
 void readDatabase(const std::string fileName);
