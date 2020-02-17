c31c120931ef78d50e89b94114b2ef5a19f29335
Jaron Cohen <CHNJAR003@myuct.ac.za>
2020-02-17 16:23:46 +0200

    Defined an initial version of the displayStudentData function in database.cpp

--- database.h
+++ database.h
@@ -20,7 +20,7 @@ extern std::vector<Student> StudentRecords;
 void addStudent(const std::string fName, const std::string sName, const std::string studentNumber, const std::string classRecord);
 void readDatabase(const std::string fileName);
 void saveDatabase(const std::string fileName);
-void displayStudentData(const std::string studentNumber);
+Student displayStudentData(const std::string studentNumber);
 void gradeStudent(const std::string studentNumber);
 Student parseFileLine(const std::string line);
 std::string serialiseStudent(const Student tempStudent);
